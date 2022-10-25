88f16de055105925767cd50b0ddad58871cc247a
Andrew Tapper <andrew@tapper.codes>
2022-10-25 14:12:48 -0500

    :sparkles: add api call with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import ReactDOM from 'react-dom'
-import Pet from './Pet'
+import { createRoot } from 'react-dom/client'
+import SearchParams from './SearchParams'
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Stella" animal="Cat" breed="Domestic" />
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Doink" animal="Bird" breed="Cockatiel" />
+      <SearchParams />
     </div>
   )
 }
 
 const container = document.getElementById('root')
-const root = ReactDOM.createRoot(container)
-root.render(App)
+const root = createRoot(container)
+root.render(<App />)
